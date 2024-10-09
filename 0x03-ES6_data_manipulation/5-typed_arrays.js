789becccd070516736b29bf35032d550e0676385
daramola oluwatoyin <victoria.mnpadm@gmail.com>
2024-10-09 05:04:28 +0100

    data manipulation

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const dv = new DataView(buffer);
+
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+
+  dv.setInt8(position, value);
+
+  return buffer;
+}
